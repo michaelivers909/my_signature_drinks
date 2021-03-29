e9afea49e7b6accbdcb5a9c0d9543e6925799b20
michaelivers909 <michael.ivers@midlandu.edu>
2021-03-28 19:58:08 -0500

    needs save, delete, and edit with db also

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -4,52 +4,21 @@ import { AuthContext } from "../contexts/AuthContext";
 import { Button, Card, Form, Alert, Container } from "react-bootstrap";
 import "../Style.css";
 
-const CreatePage = (props) => {
-  const [name, setName] = useState("");
-  const [picture, setPicture] = useState("");
-  const [mainSpirit, setMainSpirit] = useState("");
-  const [ingredients1, setIngredients1] = useState("");
-  const [ingredients2, setIngredients2] = useState("");
-  const [directions, setDirections] = useState("");
+
+const CreatePage = () => {
   const [error, setError] = useState("");
   const uploadedImage = React.useRef(null);
   const imageUploader = React.useRef(null);
-  
-  const handleName = event => {
-     const { target } = event;
-     setName(name => ({
-       ...name,
-       name: target.value
-    }))
-    };
-  const handleMainSpirit = event => {
-    const { target } = event;
-    setMainSpirit(mainSpirit => ({
-      ...mainSpirit,
-      name: target.value
-    }))
-   };
-   const handleIngredients1 = event => {
-    const { target } = event;
-    setIngredients1(ingredients1 => ({
-      ...ingredients1,
-      name: target.value
-    }))
-   };
-   const handleIngredients2 = event => {
-    const { target } = event;
-    setIngredients2(ingredients2 => ({
-      ...ingredients2,
-      name: target.value
-    }))
-   };
-   const handleDirections = event => {
-    const { target } = event;
-    setDirections(directions => ({
-      ...directions,
-      name: target.value
-    }))
-   };
+  const [recipe, setRecipe] = React.useState([
+    {
+      name: "",
+      picture: "",
+      spirit: "",
+      ingredients1: "",
+      ingredients2: "",
+      directions: "",
+    },
+  ]);
 
   const handleImageUpload = (e) => {
     const [file] = e.target.files;
@@ -63,45 +32,56 @@ const CreatePage = (props) => {
       reader.readAsDataURL(file);
     }
   };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-  const handleSubmit = () => {
-    
-  }
 
   return (
-      <body className="bgParchGrey">
-       <Container>
+    <body className="bgParchGrey">
+      <Container>
         <Form className="bgParchGrey fontM">
-            <h1 className="text-center fontDafoe">Create a Cocktail Recipe</h1>
+          <h1 className="text-center fontDafoe">Create a Cocktail Recipe</h1>
           <Form.Group>
             <Form.Label className="margin-auto">Cocktail Name</Form.Label>
-            <Form.Control value={name} className="text-center justify-element-center" style={{maxWidth: 600}} type="text" placeholder="Enter Name Of Cocktail" />
+            <Form.Control
+              className="text-center justify-element-center"
+              style={{ maxWidth: 600 }}
+              type="text"
+              placeholder="Enter Name Of Cocktail"
+              value={recipe.name}
+            />
           </Form.Group>
           <Form.Group>
-          <div className="text-center mb-4">
-            <input
-              value={picture}
-              type="file"
-              accept="image/*"
-              onChange={handleImageUpload}
-              ref={imageUploader}
-              style={{
-                display: "none",
-              }}
-            />
-            <div className="imgUpload mb-2"
-              onClick={() => imageUploader.current.click()}
-            >
-              <img className="drinkImage"
-                ref={uploadedImage}
+            <div className="text-center mb-4">
+              <input
+                value={recipe.picture}
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+                ref={imageUploader}
+                style={{
+                display: "none"
+                }}
               />
+              <div
+                className="imgUpload mb-2"
+                onClick={() => imageUploader.current.click()}
+              >
+                <img className="drinkImage" ref={uploadedImage} />
+              </div>
+              Click to upload Image
             </div>
-            Click to upload Image
-          </div>
           </Form.Group>
           <Form.Group>
             <Form.Label>Select Main Spirit</Form.Label>
-            <Form.Control value={mainSpirit} className="text-center" style={{maxWidth: 150}}as="select">
+            <Form.Control
+              className="text-center"
+              style={{ maxWidth: 150 }}
+              as="select"
+              value={recipe.spirit}
+            >
               <option>Vodka</option>
               <option>Gin</option>
               <option>Rum</option>
@@ -115,32 +95,48 @@ const CreatePage = (props) => {
           <Form.Group>
             <Form.Label>Spirits and Measurements</Form.Label>
             <Form.Control
-              value={ingredients1}
               as="textarea"
               placeholder="Enter all spirits and measurements here."
+              value={recipe.ingredients1}
             ></Form.Control>
           </Form.Group>
           <Form.Group>
-            <Form.Label >All other ingredients</Form.Label>
+            <Form.Label>All other ingredients</Form.Label>
             <Form.Control
-              value={ingredients2}
               as="textarea"
               placeholder="Enter sweet, bitter, juices, syrups, soda, egg whites, and all other ingredients & measurements here."
+              value={recipe.ingredients2}
             ></Form.Control>
           </Form.Group>
           <Form.Group>
             <Form.Label>Directions</Form.Label>
-            <Form.Control value={directions} as="textarea" placeholder="Enter directions here." />
+            <Form.Control
+              as="textarea"
+              placeholder="Enter directions here."
+              value={recipe.directions}
+            />
           </Form.Group>
         </Form>
         <div>
-          <Button className="text-center" variant="info">Save Recipe</Button>
+          {recipe.map(data => (
+            <div>
+              <p>{data.name}</p>
+              <p>{data.picture}</p>
+              <p>{data.spirit}</p>
+              <p>{data.ingredients1}</p>
+              <p>{data.ingredients2}</p>
+              <p>{data.directions}</p>
+            </div>
+          ))}
         </div>
-       
+        <div>
+          <Button type="submit" className="text-center" variant="info">
+            Save Recipe
+          </Button>
+        </div>
       </Container>
     </body>
-    
   );
-}
+};
 
 export default CreatePage;
