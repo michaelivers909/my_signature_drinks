70d6dff8bf5df181fccf9777177dd09726b21874
michaelivers909 <michael.ivers@midlandu.edu>
2021-04-09 15:08:32 -0500

    Added delete button

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -2,24 +2,23 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 import { Button, Card, Form, Alert, Container } from "react-bootstrap";
+import firebase from "../Firebase";
 import { v4 as uuidv4 } from "uuid";
 import "../Style.css";
-
 
 const CreatePage = () => {
   const [error, setError] = useState("");
   const uploadedImage = React.useRef(null);
   const imageUploader = React.useRef(null);
-  const [recipe, setRecipe] = React.useState(
-    {
-      name: "",
-      picture: "",
-      spirit: "vodka",
-      ingredients1: "",
-      ingredients2: "",
-      directions: "",
-    }
-  );
+  // const [recipe, setRecipe] = useState("");
+  const [name, setName] = useState("");
+  const [picture, setPicture] = useState("");
+  const [spirit, setSpirit] = useState("");
+  const [ingredients1, setIngredients1] = useState("");
+  const [ingredients2, setIngredients2] = useState("");
+  const [directions, setDirections] = useState("");
+
+  const db = firebase.firestore().collection("recipes");
 
   const handleImageUpload = (e) => {
     const [file] = e.target.files;
@@ -33,125 +32,119 @@ const CreatePage = () => {
       reader.readAsDataURL(file);
     }
   };
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
 
-    setRecipe(recipe => ({
-      ...recipe,
-    }));
-  };
-
+  function addRecipe(newRecipe) {
+    db.doc(newRecipe.id)
+      .set(newRecipe)
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
   return (
     <body className="bgParchGrey">
-      <Container>
-        <Form className="bgParchGrey fontM">
-          <h1 className="text-center fontDafoe">Create a Cocktail Recipe</h1>
-          <Form.Group>
-            <Form.Label className="margin-auto">Cocktail Name</Form.Label>
-            <Form.Control
-              className="text-center justify-element-center"
-              style={{ maxWidth: 600 }}
-              type="text"
-              placeholder="Enter Name Of Cocktail"
-              value={recipe.name}
-              onChange={(e) => setRecipe((val) => ({ ...val, name: e.target.value })) 
-              }
-
+    {/* <Container className="bgParchGrey"> */}
+      <Form className="bgParchGrey fontM">
+        <h1 className="text-center fontDafoe">Create a Cocktail Recipe</h1>
+        <Form.Group>
+          <Form.Label className="margin-auto">Cocktail Name</Form.Label>
+          <Form.Control
+            className="text-center justify-element-center"
+            style={{ maxWidth: 600 }}
+            type="text"
+            placeholder="Enter Name Of Cocktail"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <div className="text-center mb-4">
+            <input
+              value={picture}
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              ref={imageUploader}
+              style={{
+                display: "none",
+              }}
             />
-          </Form.Group>
-          <Form.Group>
-            <div className="text-center mb-4">
-              <input
-                value={recipe.picture}
-                type="file"
-                accept="image/*"
-                onChange={handleImageUpload}
-                ref={imageUploader}
-                style={{
-                display: "none" 
-                }}
-              />
-              <div
-                className="imgUpload mb-2"
-                onClick={() => imageUploader.current.click()}
-              >
-                <img className="drinkImage" ref={uploadedImage} />
-              </div>
-              Click to upload Image
+            <div
+              className="imgUpload mb-2"
+              onClick={() => imageUploader.current.click()}
+            >
+              <img className="drinkImage" ref={uploadedImage} />
             </div>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Select Main Spirit</Form.Label>
-            <Form.Control
-              className="text-center"
-              style={{ maxWidth: 150 }}
-              as="select"
-              value={recipe.spirit}
-              onChange={(e) => setRecipe((val) => ({ ...val, spirit: e.target.value })) }
-            >
-              <option value="vodka">Vodka</option>
-              <option value="gin">Gin</option>
-              <option value="rum">Rum</option>
-              <option value="tequila">Tequila</option>
-              <option value="bourbon">Bourbon</option>
-              <option value="scotch">Scotch</option>
-              <option value="whiskey">Misc. Whiskey</option>
-              <option value="other">Other</option>
-            </Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Spirits and Measurements</Form.Label>
-            <Form.Control
-              as="textarea"
-              placeholder="Enter all spirits and measurements here."
-              value={recipe.ingredients1}
-              onChange={(e) => setRecipe((val) => ({ ...val, ingredients1: e.target.value }))}
-            >
-            </Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>All other ingredients</Form.Label>
-            <Form.Control
-              as="textarea"
-              placeholder="Enter sweet, bitter, juices, syrups, soda, egg whites, and all other ingredients & measurements here."
-              value={recipe.ingredients2}
-              onChange={(e) => setRecipe((val) => ({ ...val, ingredients2: e.target.value }))}
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Directions</Form.Label>
-            <Form.Control
-              as="textarea"
-              placeholder="Enter directions here."
-              value={recipe.directions}
-              onChange={(e) => setRecipe((val) => ({ ...val, directions: e.target.value }))}
-            />
-          </Form.Group>
-        </Form>
-        {/* <div> */}
-          {/* {recipe.create.map((recipe) => ( */}
-            {/* // <div> */}
-              {/* <span>{recipe.name}</span> */}
-              {/* <span>{recipe.picture}</span> */}
-              {/* <span>{recipe.spirit}</span> */}
-              {/* <span>{recipe.ingredients1}</span> */}
-              {/* <span>{recipe.ingredients2}</span> */}
-              {/* <span>{recipe.directions}</span> */}
-            {/* </div> */}
-          {/* // ))} */}
-        {/* </div> */}
-        <div>
-          <Button 
-          type="submit" 
-          className="text-center" 
-          variant="info" 
-          onClick={handleSubmit}>
-            Save Recipe
-          </Button>
-        </div>
-      </Container>
+            Click to upload Image
+          </div>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Select Main Spirit</Form.Label>
+          <Form.Control
+            className="text-center"
+            style={{ maxWidth: 150 }}
+            as="select"
+            value={spirit}
+            onChange={(e) => setSpirit(e.target.value)}
+          >
+            <option value="vodka">Vodka</option>
+            <option value="gin">Gin</option>
+            <option value="rum">Rum</option>
+            <option value="tequila">Tequila</option>
+            <option value="bourbon">Bourbon</option>
+            <option value="scotch">Scotch</option>
+            <option value="whiskey">Misc. Whiskey</option>
+            <option value="other">Other</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Spirits and Measurements</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="Enter all spirits and measurements here."
+            value={ingredients1}
+            onChange={(e) => setIngredients1(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>All other ingredients</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="Enter sweet, bitter, juices, syrups, soda, egg whites, and all other ingredients & measurements here."
+            value={ingredients2}
+            onChange={(e) => setIngredients2(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Directions</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="Enter directions here."
+            value={directions}
+            onChange={(e) => setDirections(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      <div>
+        <Button
+          type="submit"
+          variant="info"
+          onClick={() =>
+            addRecipe({
+              name,
+              picture,
+              spirit,
+              ingredients1,
+              ingredients2,
+              directions,
+              id: uuidv4(),
+            })
+          }
+        >
+          Save Recipe
+        </Button>
+      </div>
+    {/* </Container> */}
     </body>
   );
 };
--- src/components/SavedRecipes.js
+++ src/components/SavedRecipes.js
@@ -6,52 +6,93 @@ import firebase from "../Firebase";
 import "../Style.css";
 
 export default function SavedRecipes() {
-    const [recipes, setRecipes] = useState([]);
-    const [loading, setLoading] = useState(false);
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const ref = firebase.firestore().collection("recipes");
-    console.log(ref);
+  const db = firebase.firestore().collection("recipes");
+  console.log(db);
 
-    function getRecipes() {
-        setLoading(true);
-        ref.onSnapshot((querySnapshot) => {
-            const items = [];
-            querySnapshot.forEach((doc) =>{
-                items.push(doc.data());
-            });
-            setRecipes(items);
-            setLoading(false);
-        });
-    }
+  function getRecipes() {
+    setLoading(true);
+    db.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setRecipes(items);
+      setLoading(false);
+    });
+  }
 
-    useEffect(() => {
-        getRecipes();
-    }, []);
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
-    // if (loading) {
-        // return <h1>Loading...</h1>;
-    // }
+  function deleteRecipe(recipe) {
+    db.doc(recipe.id)
+      .delete()
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
-    return (
-       
-        <body className="bgParchGrey"
-        style={{ minHeight: 800 }}>
-         <h1 className="text-center fontDafoe">My Saved Recipes</h1>
-            {loading ? <h1>Loading...</h1> : null}
-            {recipes.map((recipe) =>
-            <div key={recipe.id}>
-                <h2>{recipe.name}</h2>
-                <img>{recipe.picture}</img>
-                <p>{recipe.spirit}</p>
-                <p>{recipe.ingredients1}</p>
-                <p>{recipe.ingredients2}</p>
-                <p>{recipe.directions}</p>
-            </div>
+  function editRecipe(editedRecipe) {
+    setLoading();
+    db.doc(editedRecipe.id)
+      .update(editedRecipe)
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <body className="bgParchGrey" style={{ minHeight: 800 }}>
+      <h1 className="text-center fontDafoe">My Saved Recipes</h1>
+      {loading ? <h1>Loading...</h1> : null}
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <p>{recipe.picture}</p>
+          <p>{recipe.spirit}</p>
+          <p>{recipe.ingredients1}</p>
+          <p>{recipe.ingredients2}</p>
+          <p>{recipe.directions}</p>
+          {recipes > 1 && (
+        <>
+          <div>
+            <Button
+              className="m-2"
+              variant="info"
+              onClick={() => deleteRecipe(recipe)}
+            >
+              Delete Recipe
+            </Button>
+          </div>
+          <div>
+            <Button
+              className="m-2"
+              variant="info"
+              onClick={() =>
+                editRecipe(
+                  recipe.name,
+                  recipe.spirit,
+                  recipe.ingredients1,
+                  recipe.ingredients2,
+                  recipe.directions
+                )
+              }
+            >
+              Edit Recipe
+            </Button>
+            </>
             )}
-      </body>
-      
-    
-    )
-};
-
-   
+          </div>
+        </div>
+      ))}
+    </body>
+  );
+}
